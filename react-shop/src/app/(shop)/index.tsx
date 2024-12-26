858e5d9304e0d173d3c2b095515c06d4bee41ef5
DESKTOP-211ALED\user <emanoel_oliveira@outlook.com>
2024-12-26 01:13:51 -0300

    initial commit

---
+++ react-shop/src/app/(shop)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View> //view component that holdes a text component that can show a text
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
