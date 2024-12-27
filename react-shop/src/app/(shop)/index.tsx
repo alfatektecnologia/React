392d77400715e4d80ec3908c3f822349c2c5e078
DESKTOP-211ALED\user <emanoel_oliveira@outlook.com>
2024-12-27 08:38:41 -0300

    Added pages

--- react-shop/src/app/(shop)/index.tsx
+++ react-shop/src/app/(shop)/index.tsx
@@ -1,13 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import {PRODUCTS} from '../../../assets/products';
+import ProductListItem from '../../components/product-list-item';
+
 
 const Home = () => {
   return (
     <View> //view component that holdes a text component that can show a text
-      <Text>Home</Text>
+      <FlatList 
+      data={PRODUCTS}
+      renderItem={({item})=> <ProductListItem product={item}/>}
+      keyExtractor={item => item.id.toString()}
+      numColumns={2}
+      ListHeaderComponent={<Text>Products</Text>}
+      contentContainerStyle={styles.flatListContent}
+      columnWrapperStyle={styles.flatListColumn}
+      style={{ paddingHorizontal:10, paddingVertical:5}}
+      />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom:20,
+  },
+  flatListColumn: {
+    justifyContent:'space-between',
+  },
+
+})
